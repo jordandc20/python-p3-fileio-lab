958f968aaccdc59fb2e51fc0f6cfcff948e629f1
Diana Jordan <dianacjordan@gmail.com>
2023-04-07 16:22:26 -0400

    complte lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as file:
+        file.write(file_content)
+
+# write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as file:
+        file.write(append_content)
+
+# append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', encoding='utf-8') as file:
+        return file.read()
+
+read_file(file_name="logfile")
+
+
+
+# read_file(file_name="logfile")
